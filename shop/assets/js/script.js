ca86488d369009224f82c58dc03a862aafc3c05a
Sina <bekarsina@gmail.com>
2022-02-01 00:23:09 +0330

    New Updata for login page

--- shop/assets/js/script.js
+++ shop/assets/js/script.js
@@ -13,7 +13,7 @@ function myFunctionClose() {
   }
 }
 
-// from line 18 to 35 for add to card product
+// from line 18 to 24 is for add to card product
 var ValueAddP = 0;
 var New;
 
@@ -22,6 +22,8 @@ function myFuncAddNum() {
   New = Add.innerHTML = Number(Add.innerHTML) + 1;
 }
 
+
+// frome line 27 to 39 is for login and regester form
 var Reg = $.querySelector(".regester");
 var Login = $.querySelector(".login");
 reg.style.display = "none";
@@ -34,4 +36,26 @@ function regester() {
 function login() {
   Login.style.display = "flex";
   Reg.style.display = "none";
+}
+
+var userNameInput = $.querySelector(".userName");
+var userNameError = $.querySelector(".userNameError");
+
+var userPasswordInput = $.querySelector(".userPassword");
+var userpasswordError = $.querySelector(".userpasswordError");
+
+function userNameValidate(){
+  if (userNameInput.value.length < 8) {
+    userNameError.innerHTML = "نام کاربری شما باید حداقل 8 کارکتر داشته باشد";
+  }else{
+    userNameError.innerHTML = "";
+  }
+}
+
+function userPasswordValidate(){
+  if (userPasswordInput.value.length < 8) {
+    userpasswordError.innerHTML = "رمز عبور شما باید حداقل 8 کارکتر داشته باشد";
+  }else{
+    userpasswordError.innerHTML = "";
+  }
 }
