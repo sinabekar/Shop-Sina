068b0faf3eb601826ef5ea308d2d48037a8597f1
Sina <bekarsina@gmail.com>
2022-02-05 11:40:15 +0330

    New Updata for hamber menu

--- shop/assets/js/script.js
+++ shop/assets/js/script.js
@@ -1,16 +1,9 @@
 var $ = document;
 
-// from line 4 to 14 for hamber menu
-function myFunction() {
-  var hamber = $.querySelector(".main-hamber");
-  hamber.style.display = "flex";
-}
-
-function myFunctionClose() {
-  var hamber = $.querySelector(".main-hamber");
-  if (hamber.style.display == "flex") {
-    hamber.style.display = "none";
-  }
+// from line 4 to 7 for hamber menu
+var hamber = $.querySelector(".main-hamber");
+function hamberMenu() {
+  hamber.classList.toggle("display");
 }
 
 // from line 18 to 24 is for add to card product
@@ -21,7 +14,6 @@ function myFuncAddNum() {
   var Add = $.querySelector(".icon-shop-num-p");
   New = Add.innerHTML = Number(Add.innerHTML) + 1;
 }
-
 
 // frome line 27 to 39 is for login and regester form
 var Reg = $.querySelector(".regester");
@@ -44,18 +36,18 @@ var userNameError = $.querySelector(".userNameError");
 var userPasswordInput = $.querySelector(".userPassword");
 var userpasswordError = $.querySelector(".userpasswordError");
 
-function userNameValidate(){
+function userNameValidate() {
   if (userNameInput.value.length < 8) {
     userNameError.innerHTML = "نام کاربری شما باید حداقل 8 کارکتر داشته باشد";
-  }else{
+  } else {
     userNameError.innerHTML = "";
   }
 }
 
-function userPasswordValidate(){
+function userPasswordValidate() {
   if (userPasswordInput.value.length < 8) {
     userpasswordError.innerHTML = "رمز عبور شما باید حداقل 8 کارکتر داشته باشد";
-  }else{
+  } else {
     userpasswordError.innerHTML = "";
   }
 }
