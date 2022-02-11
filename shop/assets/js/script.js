fd39daa557163aea761d9dc34e884e48587af6e6
Sina <bekarsina@gmail.com>
2022-02-11 15:24:15 +0330

    Install fontAwesome and creat glibal script

--- shop/assets/js/script.js
+++ shop/assets/js/script.js
@@ -1,7 +1,19 @@
 var $ = document;
 
+function _id(id_name) {
+  return $.getElementById(id_name);
+}
+
+function _class(class_name) {
+  return $.getElementsByClassName(class_name);
+}
+
+function _query(class_name) {
+  return $.querySelector(class_name);
+}
+
 // from line 4 to 7 for hamber menu
-var hamber = $.querySelector(".main-hamber");
+var hamber = _query(".main-hamber");
 function hamberMenu() {
   hamber.classList.toggle("display");
 }
@@ -11,13 +23,13 @@ var ValueAddP = 0;
 var New;
 
 function myFuncAddNum() {
-  var Add = $.querySelector(".icon-shop-num-p");
+  var Add = _query(".icon-shop-num-p");
   New = Add.innerHTML = Number(Add.innerHTML) + 1;
 }
 
 // frome line 18 to 53 is for login and regester form
-var Reg = $.querySelector(".regester");
-var Login = $.querySelector(".login");
+var Reg = _query(".regester");
+var Login = _query(".login");
 reg.style.display = "none";
 
 function regester() {
@@ -30,11 +42,11 @@ function login() {
   Reg.style.display = "none";
 }
 
-var userNameInput = $.querySelector(".userName");
-var userNameError = $.querySelector(".userNameError");
+var userNameInput = _query(".userName");
+var userNameError = _query(".userNameError");
 
-var userPasswordInput = $.querySelector(".userPassword");
-var userpasswordError = $.querySelector(".userpasswordError");
+var userPasswordInput = _query(".userPassword");
+var userpasswordError = _query(".userpasswordError");
 
 function userNameValidate() {
   if (userNameInput.value.length < 8) {
